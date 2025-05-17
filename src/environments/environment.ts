06e0d7b065cc19f4d37ce886fbcdd53b3e935e45
JuanPescoran <118696670+JuanPescoran@users.noreply.github.com>
2025-05-17 16:25:07 -0500

    chore(environments): change the url for deployed api connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8080/api", // Asegúrate de que esta URL sea correcta para tu backend Spring Boot
+  apiUrl: "https://teemo-backend-production.up.railway.app/api", // Asegúrate de que esta URL sea correcta para tu backend Spring Boot
   mockBackend: false, // Activar modo de prueba cuando no hay backend real
 }
