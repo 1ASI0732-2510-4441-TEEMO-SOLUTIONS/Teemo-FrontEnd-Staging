5f78304e6f044430a2716b9346b70cae0efb9635
JoseRiega <u202211254@upc.edu.pe>
2025-07-11 21:17:22 -0500

    feat: change environment url 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: "https://your-production-api.com", // Cambia esto a la URL de tu API de producción
-  mockBackend: false, // En producción, usar el backend real
+  //apiUrl: "https://your-production-api.com"
+  mockBackend: false,
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: "https://teemo-backend-production.up.railway.app/api",
+  apiUrl: "mongodb+srv://teemo:u202214522@teemo.ck7qain.mongodb.net/TeemoDB?retryWrites=true&w=majority",
   //apiUrl: "https://teemo-backend-production.up.railway.app/api"
   mockBackend: false,
 }
